bc7191337b4aaad83c8e93db95c0bfd21cc9fbc1
liam <liam@scalzulli.com>
2025-03-22 22:13:45 -0400

    Analyze value identifiers in expressions (#21)

--- src/text_node.rs
+++ src/text_node.rs
@@ -5,19 +5,3 @@ pub(crate) struct TextNode {
   pub(crate) value: String,
   pub(crate) range: lsp::Range,
 }
-
-impl TextNode {
-  pub(crate) fn is_quoted(&self) -> bool {
-    if self.value.len() < 2 {
-      return false;
-    }
-
-    matches!(
-      (
-        self.value.chars().next().unwrap(),
-        self.value.chars().last().unwrap()
-      ),
-      ('"', '"') | ('\'', '\'')
-    )
-  }
-}
