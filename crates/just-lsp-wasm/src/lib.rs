b026f5f33732e58976b0146c0526f00e3103139e
Liam <liam@scalzulli.com>
2026-04-13 18:41:12 -0400

    Add initial work

--- crates/just-lsp-wasm/src/lib.rs
+++ crates/just-lsp-wasm/src/lib.rs
@@ -1,15 +1,43 @@
-#[must_use]
-pub fn add(left: u64, right: u64) -> u64 {
-  left + right
+use {
+  just_lsp::{Analyzer, Document},
+  lsp_types::DiagnosticSeverity,
+  serde::Serialize,
+  wasm_bindgen::prelude::*,
+};
+
+#[derive(Serialize)]
+struct Diagnostic {
+  id: String,
+  message: String,
+  severity: &'static str,
+  start_line: u32,
+  start_character: u32,
+  end_line: u32,
+  end_character: u32,
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
+#[wasm_bindgen]
+pub fn analyze(source: &str) -> JsValue {
+  serde_wasm_bindgen::to_value(
+    &Analyzer::from(&Document::from(source))
+      .analyze()
+      .into_iter()
+      .map(|diagnostic| Diagnostic {
+        id: diagnostic.id,
+        message: diagnostic.message,
+        severity: match diagnostic.severity {
+          DiagnosticSeverity::ERROR => "error",
+          DiagnosticSeverity::WARNING => "warning",
+          DiagnosticSeverity::INFORMATION => "information",
+          DiagnosticSeverity::HINT => "hint",
+          _ => "unknown",
+        },
+        start_line: diagnostic.range.start.line,
+        start_character: diagnostic.range.start.character,
+        end_line: diagnostic.range.end.line,
+        end_character: diagnostic.range.end.character,
+      })
+      .collect::<Vec<_>>(),
+  )
+  .unwrap()
 }
