7082dcb065e6daa439c76a74cc075ff873e3e3d7
Liam <liam@scalzulli.com>
2026-05-01 22:38:23 -0400

    Format

--- www/src/components/about-dialog.tsx
+++ www/src/components/about-dialog.tsx
@@ -1,9 +1,5 @@
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 
 export const AboutDialog = () => {
   return (
