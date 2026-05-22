0749bd2896921114b72dc313f8a0ef15cd66ebda
Liam <liam@scalzulli.com>
2026-05-22 12:20:57 -0400

    Expose analyzer diagnostics from just-lsp-wasm crate

--- crates/just-lsp-wasm/src/lib.rs
+++ crates/just-lsp-wasm/src/lib.rs
@@ -1,15 +1,48 @@
-#[must_use]
-pub fn add(left: u64, right: u64) -> u64 {
-  left + right
+use {
+  just_lsp::{Analyzer, Document},
+  lsp_types::DiagnosticSeverity,
+  serde::Serialize,
+  typeshare::typeshare,
+  wasm_bindgen::prelude::*,
+};
+
+#[derive(Serialize)]
+#[typeshare]
+struct Diagnostic {
+  end_character: u32,
+  end_line: u32,
+  id: String,
+  message: String,
+  severity: &'static str,
+  start_character: u32,
+  start_line: u32,
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
+/// # Errors
+///
+/// Returns a `JsError` if serialization of diagnostics fails.
+#[wasm_bindgen]
+pub fn analyze(source: &str) -> Result<JsValue, JsError> {
+  serde_wasm_bindgen::to_value(
+    &Analyzer::from(&Document::from(source))
+      .analyze()
+      .into_iter()
+      .map(|diagnostic| Diagnostic {
+        end_character: diagnostic.range.end.character,
+        end_line: diagnostic.range.end.line,
+        id: diagnostic.id,
+        message: diagnostic.message,
+        severity: match diagnostic.severity {
+          DiagnosticSeverity::ERROR => "error",
+          DiagnosticSeverity::HINT => "hint",
+          DiagnosticSeverity::INFORMATION => "information",
+          DiagnosticSeverity::WARNING => "warning",
+          _ => "unknown",
+        },
+        start_character: diagnostic.range.start.character,
+        start_line: diagnostic.range.start.line,
+      })
+      .collect::<Vec<_>>(),
+  )
+  .map_err(|error| JsError::new(&error.to_string()))
 }
