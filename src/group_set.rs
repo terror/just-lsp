9a780a6234c82490e936b8485c8077ed520115a6
Liam <liam@scalzulli.com>
2026-07-27 16:46:39 -0400

    Add tests

--- src/group_set.rs
+++ src/group_set.rs
@@ -78,3 +78,122 @@ impl FromIterator<Group> for GroupSet {
     Self(iter.into_iter().collect())
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn conflicts_with() {
+    assert!(
+      !GroupSet::from([Group::Linux])
+        .conflicts_with(&GroupSet::from([Group::Windows]))
+    );
+
+    assert!(
+      GroupSet::from([Group::Linux])
+        .conflicts_with(&GroupSet::from([Group::Linux]))
+    );
+
+    assert!(
+      GroupSet::from([Group::Any])
+        .conflicts_with(&GroupSet::from([Group::Windows]))
+    );
+  }
+
+  #[test]
+  fn from_attribute() {
+    for (attribute, expected) in [
+      ("android", Some(GroupSet::from([Group::Android]))),
+      ("dragonfly", Some(GroupSet::from([Group::Dragonfly]))),
+      ("freebsd", Some(GroupSet::from([Group::Freebsd]))),
+      ("linux", Some(GroupSet::from([Group::Linux]))),
+      ("macos", Some(GroupSet::from([Group::Macos]))),
+      ("netbsd", Some(GroupSet::from([Group::Netbsd]))),
+      ("openbsd", Some(GroupSet::from([Group::Openbsd]))),
+      (
+        "unix",
+        Some(GroupSet::from([
+          Group::Android,
+          Group::Dragonfly,
+          Group::Freebsd,
+          Group::Linux,
+          Group::Macos,
+          Group::Netbsd,
+          Group::Openbsd,
+        ])),
+      ),
+      ("windows", Some(GroupSet::from([Group::Windows]))),
+      ("foo", None),
+    ] {
+      assert_eq!(GroupSet::from_attribute(attribute), expected);
+    }
+  }
+
+  #[test]
+  fn from_attributes() {
+    assert_eq!(GroupSet::from_attributes(&[]), GroupSet::from([Group::Any]));
+
+    assert_eq!(
+      GroupSet::from_attributes(&[
+        Attribute {
+          name: TextNode {
+            value: "linux".into(),
+            ..Default::default()
+          },
+          ..Default::default()
+        },
+        Attribute {
+          name: TextNode {
+            value: "private".into(),
+            ..Default::default()
+          },
+          ..Default::default()
+        },
+        Attribute {
+          name: TextNode {
+            value: "windows".into(),
+            ..Default::default()
+          },
+          ..Default::default()
+        },
+      ]),
+      GroupSet::from([Group::Linux, Group::Windows])
+    );
+  }
+
+  #[test]
+  fn from_iterator() {
+    assert_eq!(
+      [Group::Linux, Group::Linux, Group::Windows]
+        .into_iter()
+        .collect::<GroupSet>(),
+      GroupSet::from([Group::Linux, Group::Windows])
+    );
+  }
+
+  #[test]
+  fn insert() {
+    let mut groups = GroupSet::default();
+
+    assert!(groups.insert(Group::Linux));
+    assert!(!groups.insert(Group::Linux));
+
+    assert_eq!(groups, GroupSet::from([Group::Linux]));
+  }
+
+  #[test]
+  fn is_empty() {
+    assert!(GroupSet::default().is_empty());
+    assert!(!GroupSet::from([Group::Linux]).is_empty());
+  }
+
+  #[test]
+  fn union_with() {
+    let mut groups = GroupSet::from([Group::Linux]);
+
+    groups.union_with(GroupSet::from([Group::Linux, Group::Windows]));
+
+    assert_eq!(groups, GroupSet::from([Group::Linux, Group::Windows]));
+  }
+}
