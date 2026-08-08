c1cc2f71926c06404c16b9abbe2e4b8c9491b6f9
عبد الرحمن صباهي <oss@mail.ar-ms.me>
2026-08-08 20:33:08 +0300

    add test

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -4056,6 +4056,20 @@ mod tests {
   }
 
   #[test]
+  fn underscore_suppresses_unused_variable_warnings() {
+    Test::new(indoc! {
+      "
+      _ := 'foo'
+      _bar := 'baz'
+
+      recipe:
+        echo 'Hello!'
+      "
+    })
+    .run();
+  }
+
+  #[test]
   fn settings_array_type_error() {
     Test::new(indoc! {
       "
