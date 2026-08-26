6a6f42553d540f6bad81e7c0a651ee9e8c738332
Liam <liam@scalzulli.com>
2026-08-26 00:57:41 -0400

    Order quickfix tests

--- src/quickfixer.rs
+++ src/quickfixer.rs
@@ -285,15 +285,6 @@ mod tests {
   }
 
   #[test]
-  fn skips_windows_shell_setting_when_replacement_exists() {
-    Test::new(
-      "[windows]\nset shell := [\"foo\"]\nset windows-shell := [\"bar\"]\n",
-    )
-    .range(lsp::Range::at(2, 4, 2, 4))
-    .run();
-  }
-
-  #[test]
   fn skips_disabled_rules() {
     let config = serde_json::from_value::<Config>(serde_json::json!({
       "rules": {
@@ -306,5 +297,14 @@ mod tests {
       .config(config)
       .range(lsp::Range::at(0, 10, 0, 10))
       .run();
+  }
+
+  #[test]
+  fn skips_windows_shell_setting_when_replacement_exists() {
+    Test::new(
+      "[windows]\nset shell := [\"foo\"]\nset windows-shell := [\"bar\"]\n",
+    )
+    .range(lsp::Range::at(2, 4, 2, 4))
+    .run();
   }
 }
