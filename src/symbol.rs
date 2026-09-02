3ad946688fc65e386c9387c97084e43073f5a535
Liam <liam@scalzulli.com>
2026-09-02 12:56:33 -0400

    Reject rename of non-renameable identifiers

--- src/symbol.rs
+++ src/symbol.rs
@@ -8,3 +8,17 @@ pub(crate) enum Symbol {
   Recipe(Located<Recipe>),
   Variable(Located<Variable>),
 }
+
+impl Symbol {
+  #[must_use]
+  pub(crate) fn is_renameable(&self) -> bool {
+    matches!(
+      self,
+      Self::Function(_)
+        | Self::FunctionParameter(_)
+        | Self::Parameter(_)
+        | Self::Recipe(_)
+        | Self::Variable(_)
+    )
+  }
+}
