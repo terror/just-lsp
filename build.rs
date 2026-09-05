8532a1df2aab0bacbfbbcef482149f020c41fd82
liam <liam@scalzulli.com>
2026-09-04 22:22:41 -0400

    Flatten build script (#614)

--- build.rs
+++ build.rs
@@ -1,87 +1,14 @@
-use {
-  cc::Build,
-  std::path::{Path, PathBuf},
-};
-
-struct Parser<'a> {
-  extra: Vec<&'a str>,
-  name: &'a str,
-  src: &'a str,
-}
-
-impl Parser<'_> {
-  fn build(&self) {
-    let path = PathBuf::from(self.src);
-
-    let mut files = vec!["parser.c"];
-    files.extend(self.extra.clone());
-
-    let c = files
-      .iter()
-      .filter(|file| {
-        Path::new(file)
-          .extension()
-          .is_some_and(|extension| extension.eq_ignore_ascii_case("c"))
-      })
-      .copied()
-      .collect::<Vec<&str>>();
-
-    let mut build = Build::new();
-    build.include(&path).warnings(false);
-
-    for file in &c {
-      build.file(path.join(file));
-    }
-
-    build.compile(self.name);
-
-    let cpp = files
-      .iter()
-      .filter(|file| {
-        !Path::new(file)
-          .extension()
-          .is_some_and(|extension| extension.eq_ignore_ascii_case("c"))
-      })
-      .copied()
-      .collect::<Vec<&str>>();
-
-    if !cpp.is_empty() {
-      let mut build = cc::Build::new();
-
-      build
-        .include(&path)
-        .warnings(false)
-        .cpp(true)
-        .flag_if_supported("-Wno-implicit-fallthrough")
-        .flag_if_supported("-Wno-unused-parameter")
-        .flag_if_supported("-Wno-ignored-qualifiers")
-        .flag_if_supported("-Wno-return-type");
-
-      build.flag(if cfg!(windows) {
-        "/std:c++14"
-      } else {
-        "--std=c++14"
-      });
-
-      for file in &cpp {
-        build.file(path.join(file));
-      }
-
-      build.compile(&format!("{}-cpp", self.name));
-    }
-  }
-}
+use {cc::Build, std::path::Path};
 
 fn main() {
-  let parsers = vec![Parser {
-    name: "tree-sitter-just",
-    src: "vendor/tree-sitter-just-src",
-    extra: vec!["scanner.c"],
-  }];
+  let src = Path::new("vendor/tree-sitter-just-src");
 
-  for parser in &parsers {
-    println!("cargo:rerun-if-changed={}", parser.src);
-  }
+  println!("cargo:rerun-if-changed={}", src.display());
 
-  parsers.iter().for_each(Parser::build);
+  Build::new()
+    .include(src)
+    .warnings(false)
+    .file(src.join("parser.c"))
+    .file(src.join("scanner.c"))
+    .compile("tree-sitter-just");
 }
