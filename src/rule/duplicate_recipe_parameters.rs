2c977de87d87353c53cf6a4540c03de9c638c78a
Liam <liam@scalzulli.com>
2026-09-09 13:03:06 -0400

    Remove redundant variadic parameter ordering checks

--- src/rule/duplicate_recipe_parameters.rs
+++ src/rule/duplicate_recipe_parameters.rs
@@ -12,9 +12,9 @@ define_rule! {
       for recipe in context.recipes() {
         let mut seen = HashSet::new();
 
-        let (mut passed_default, mut passed_variadic) = (false, false);
+        let mut passed_default = false;
 
-        for (index, param) in recipe.parameters.iter().enumerate() {
+        for param in &recipe.parameters {
           if !seen.insert(param.name.clone()) {
             diagnostics.push(Diagnostic::error(
               format!("Duplicate parameter `{}`", param.name),
@@ -23,20 +23,6 @@ define_rule! {
           }
 
           let has_default = param.default_value.is_some();
-
-          if matches!(param.kind, ParameterKind::Variadic(_)) {
-            if index < recipe.parameters.len() - 1 {
-              diagnostics.push(Diagnostic::error(
-                format!(
-                  "Variadic parameter `{}` must be the last parameter",
-                  param.name
-                ),
-                param.range,
-              ));
-            }
-
-            passed_variadic = true;
-          }
 
           if passed_default
             && !has_default
@@ -47,13 +33,6 @@ define_rule! {
                 "Required parameter `{}` follows a parameter with a default value",
                 param.name
               ),
-              param.range,
-            ));
-          }
-
-          if passed_variadic && index < recipe.parameters.len() - 1 {
-            diagnostics.push(Diagnostic::error(
-              format!("Parameter `{}` follows a variadic parameter", param.name),
               param.range,
             ));
           }
