7192147a5ff719a4ab133a8bbcdfdaf4dd71bbaa
Liam <liam@scalzulli.com>
2026-09-09 22:57:02 -0400

    Rename `attribute-argument-expressions` rule to `invalid-attribute-argument-expression`

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5233,6 +5233,10 @@ mod tests {
       "duplicate-recipe-parameters",
       "foo bar bar:\n  echo {{bar}}\n",
     );
+    case(
+      "invalid-attribute-argument-expression",
+      "[group('foo' + 'bar')]\nfoo:\n",
+    );
     case("list-feature-gate", "foo:\n  echo {{bool('bar')}}\n");
     case("unused-recipe-parameters", "foo bar:\n");
     case("unused-variables", "foo := 'bar'\n");
