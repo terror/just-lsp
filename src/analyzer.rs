02fee2229058f97e0240ce4f2ba3e3f077a33b89
Liam <liam@scalzulli.com>
2026-09-09 22:57:17 -0400

    Rename `mapped-dependencies` rule to `invalid-mapped-dependency`

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5233,6 +5233,10 @@ mod tests {
       "duplicate-recipe-parameters",
       "foo bar bar:\n  echo {{bar}}\n",
     );
+    case(
+      "invalid-mapped-dependency",
+      "set unstable\nset lists\nfoo bar:\n  echo {{bar}}\nbaz: *(foo 'bar')\n",
+    );
     case("list-feature-gate", "foo:\n  echo {{bool('bar')}}\n");
     case("unused-recipe-parameters", "foo bar:\n");
     case("unused-variables", "foo := 'bar'\n");
