7f76f079ff13d4653fc00745f5c1a1cc8f4f178d
Liam <liam@scalzulli.com>
2026-09-09 22:56:00 -0400

    Rename `dependency-arguments` rule to `invalid-dependency-argument-count`

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5233,6 +5233,10 @@ mod tests {
       "duplicate-recipe-parameters",
       "foo bar bar:\n  echo {{bar}}\n",
     );
+    case(
+      "invalid-dependency-argument-count",
+      "foo bar:\n  echo {{bar}}\nbaz: foo\n",
+    );
     case("list-feature-gate", "foo:\n  echo {{bool('bar')}}\n");
     case("unused-recipe-parameters", "foo bar:\n");
     case("unused-variables", "foo := 'bar'\n");
