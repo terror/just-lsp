6ff72602dbf1d1cec89e262de07521f22cb69a9d
Liam <liam@scalzulli.com>
2026-09-09 22:55:34 -0400

    Rename `function-arguments` rule to `invalid-function-argument-count`

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5233,6 +5233,10 @@ mod tests {
       "duplicate-recipe-parameters",
       "foo bar bar:\n  echo {{bar}}\n",
     );
+    case(
+      "invalid-function-argument-count",
+      "foo:\n  echo {{arch('bar')}}\n",
+    );
     case("list-feature-gate", "foo:\n  echo {{bool('bar')}}\n");
     case("unused-recipe-parameters", "foo bar:\n");
     case("unused-variables", "foo := 'bar'\n");
