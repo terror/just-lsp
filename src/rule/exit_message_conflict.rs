bf98ba07f893e8db8a4d693601087813dc498051
Liam <liam@scalzulli.com>
2026-09-09 22:32:44 -0400

    Share recipe attribute conflict checks

--- src/rule/exit_message_conflict.rs
+++ src/rule/exit_message_conflict.rs
@@ -5,27 +5,7 @@ define_rule! {
     id: "exit-message-conflict",
     message: "conflicting exit message attributes",
     run(context) {
-      let mut diagnostics = Vec::new();
-
-      for recipe in context.local_declarations(context.recipes()) {
-        let exit_message_attribute = recipe.find_attribute("exit-message");
-
-        let no_exit_message_attribute = recipe.find_attribute("no-exit-message");
-
-        if let (Some(attribute), Some(_)) =
-          (exit_message_attribute, no_exit_message_attribute)
-        {
-          diagnostics.push(Diagnostic::error(
-            format!(
-              "Recipe `{}` can't combine `[exit-message]` with `[no-exit-message]`",
-              recipe.name.value
-            ),
-            attribute.range,
-          ));
-        }
-      }
-
-      diagnostics
+      context.conflicting_recipe_attributes("exit-message", "no-exit-message")
     }
   }
 }
--- src/rule/script_shell_conflict.rs
+++ src/rule/script_shell_conflict.rs
@@ -5,27 +5,7 @@ define_rule! {
     id: "script-shell-conflict",
     message: "conflicting script attributes",
     run(context) {
-      let mut diagnostics = Vec::new();
-
-      for recipe in context.local_declarations(context.recipes()) {
-        let script_attribute = recipe.find_attribute("script");
-
-        let shell_attribute = recipe.find_attribute("shell");
-
-        if let (Some(attribute), Some(_)) =
-          (script_attribute, shell_attribute)
-        {
-          diagnostics.push(Diagnostic::error(
-            format!(
-              "Recipe `{}` can't combine `[script]` with `[shell]`",
-              recipe.name.value
-            ),
-            attribute.range,
-          ));
-        }
-      }
-
-      diagnostics
+      context.conflicting_recipe_attributes("script", "shell")
     }
   }
 }
