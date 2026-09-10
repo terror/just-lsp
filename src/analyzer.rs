cf8599d0c46152d85136044c2ca9bf22a7e10159
Liam <liam@scalzulli.com>
2026-09-09 22:56:07 -0400

    Rename `attribute-arguments` rule to `invalid-attribute-argument-count`

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -5233,6 +5233,7 @@ mod tests {
       "duplicate-recipe-parameters",
       "foo bar bar:\n  echo {{bar}}\n",
     );
+    case("invalid-attribute-argument-count", "[group]\nfoo:\n");
     case("list-feature-gate", "foo:\n  echo {{bool('bar')}}\n");
     case("unused-recipe-parameters", "foo bar:\n");
     case("unused-variables", "foo := 'bar'\n");
